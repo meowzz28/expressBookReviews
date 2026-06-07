9b49addb3d6489891ec71065044b945290d6bb8d
meowzz28 <rogerlim799@gmail.com>
2026-06-07 05:52:22 -0400

    Task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -41,21 +41,29 @@ public_users.get('/', async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    // Retrieve the ISBN from the request parameters
+public_users.get('/isbn/:isbn', async function (req, res) {
     const isbn = req.params.isbn;
-    
-    // Fetch the book from the books database
-    const book = books[isbn];
-    
-    if (book) {
-        // If the book exists, return it neatly formatted
-        return res.status(200).send(JSON.stringify(book, null, 4));
-    } else {
-        // If the book is not found, return a 404 Not Found error
-        return res.status(404).json({message: "Book not found"});
+
+    try {
+        // Simulating an asynchronous operation using a Promise
+        const getBook = new Promise((resolve, reject) => {
+            const book = books[isbn];
+            if (book) {
+                resolve(book);
+            } else {
+                reject(new Error("Book not found"));
+            }
+        });
+
+        // Await the resolution of the Promise
+        const bookDetails = await getBook;
+        return res.status(200).send(JSON.stringify(bookDetails, null, 4));
+
+    } catch (error) {
+        // Catch the rejection from the Promise and return a 404 status
+        return res.status(404).json({ message: error.message });
     }
- });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
